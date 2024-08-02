f6977aa8a502088245f03d8d1f4d3fabd8b912e6
tertun <tertun@tutamail.com>
2024-08-02 15:00:09 +0300

    update configs

---
+++ IItemGroupService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace ICD.Base.BusinessServiceContract
+{
+    public interface IItemGroupService
+    {
+        Task<InsertItemGroupResult> InsertItemGroupAsync(InsertItemGroupRequest request);
+        Task<GetItemGroupsResult> GetItemGroupsByApplicationRefAsync(GetItemGroupsQuery query);
+        Task<GetItemGroupsByKeyResult> GetItemGroupsByKeyAsync(GetItemGroupsByKeyQuery query);
+        Task<DeleteTypeIntResult> DeleteItemGroupByIdAsync(DeleteTypeIntRequest request);
+        Task<BaseItemGroupResult> UpdateItemGroupByid(UpdateItemGroupRequest request);
+    }
+}
---
+++ IItemRowService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace ICD.Base.BusinessServiceContract
+{
+    public interface IItemRowService
+    {
+        Task<InsertItemRowResult> InsertItemRowAsync(InsertItemRowRequest request);
+        Task<GetItemRowsResult> GetItemRowsAsync(GetItemRowsQuery query);
+        Task<DeleteItemRowResult> DeleteItemRowByIdAsync(DeleteItemRowRequest request);
+    }
+}
